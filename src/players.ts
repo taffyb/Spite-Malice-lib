3da3b5294fb6c0a3c26a81cd240131377de4f37b
Taffy Brecknock <brecknockt@gmail.com>
2020-04-26 13:27:59 +0100

    +games +moves +players

---
+++ src/players.ts
@@ -0,0 +1,17 @@
+import { v4 as uuid } from 'uuid';
+
+export interface IPlayerModel{
+    uuid:string;
+    name:string;
+}
+export class Player implements IPlayerModel{
+    uuid: string;
+    name: string;
+}
+export class PlayerFactory{
+    static playerFromObj(obj){
+        const player:Player = new Player();
+        player.uuid = obj.uuid || uuid();
+        player.name = obj.name || 'Player';
+    }
+}
